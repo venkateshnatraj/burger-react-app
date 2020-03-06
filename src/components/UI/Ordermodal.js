c4363092ae8aeb643d08d2154504ef07c4b27ca2
venkateshnatraj <venkateshnatraj@users.noreply.github.com>
2020-03-05 23:05:25 -0500

    usememo on avoid rerendering

--- src/components/UI/Ordermodal.js
+++ src/components/UI/Ordermodal.js
@@ -1,34 +1,70 @@
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useMemo } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import { store } from '../../store/store'
 
+
+
 const Ordermodal = ({ modal, cancel, continueNext }) => {
   const { state } = useContext(store)
-  const items = Object.keys(state.ingredients).map((igkey) => (
-    <p>
-      {igkey} : {state.ingredients[igkey]}
-    </p>
-  ))
-  return (
-    <Fragment>
-      <Modal isOpen={modal} toggle={cancel}>
-        <ModalHeader toggle={cancel}>Order Summary</ModalHeader>
-        <ModalBody>
-          <h5>Your delicious Burger with following Ingrdients</h5>
-          <ul>{items}</ul>
-          <h5>Total Price : {state.total}</h5>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={() => continueNext()}>
-            Continue
-          </Button>
-          <Button color="secondary" onClick={() => cancel()}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </Fragment>
-  )
+  return useMemo(()=>{
+    const items = Object.keys(state.ingredients).map((igkey) => (
+      <p>
+        {igkey} : {state.ingredients[igkey]}
+      </p>
+    ))
+    return (
+      <Fragment>
+        <Modal isOpen={modal} toggle={cancel}>
+          <ModalHeader toggle={cancel}>Order Summary</ModalHeader>
+          <ModalBody>
+            <h5>Your delicious Burger with following Ingrdients</h5>
+            <ul>{items}</ul>
+            <h5>Total Price : {state.total}</h5>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => continueNext()}>
+              Continue
+            </Button>
+            <Button color="secondary" onClick={() => cancel()}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </Fragment>
+    )
+  },[modal])
+
+
+
+  // const { state } = useContext(store)
+  // const items = Object.keys(state.ingredients).map((igkey) => (
+  //   <p>
+  //     {igkey} : {state.ingredients[igkey]}
+  //   </p>
+  // ))
+  // console.log('test1')
+  // return (
+  //   <Fragment>
+  //     <Modal isOpen={modal} toggle={cancel}>
+  //       <ModalHeader toggle={cancel}>Order Summary</ModalHeader>
+  //       <ModalBody>
+  //         <h5>Your delicious Burger with following Ingrdients</h5>
+  //         <ul>{items}</ul>
+  //         <h5>Total Price : {state.total}</h5>
+  //       </ModalBody>
+  //       <ModalFooter>
+  //         <Button color="primary" onClick={() => continueNext()}>
+  //           Continue
+  //         </Button>
+  //         <Button color="secondary" onClick={() => cancel()}>
+  //           Cancel
+  //         </Button>
+  //       </ModalFooter>
+  //     </Modal>
+  //   </Fragment>
+  // )
+
 }
+
 
 export default Ordermodal
